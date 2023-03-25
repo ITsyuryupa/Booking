10daadccd4ce951ec88815f580b46b4f4afab1b1
i.tsyuryupa <openingthedoor@yandex.ru>
2023-03-25 14:10:14 +0300

    Extend hotel model

--- src/main/java/com/today/here/booking/model/Hotel.java
+++ src/main/java/com/today/here/booking/model/Hotel.java
@@ -2,6 +2,9 @@ package com.today.here.booking.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "hotel")
 public class Hotel {
@@ -22,10 +25,56 @@ public class Hotel {
     @Column(name = "street")
     private String street;
 
-    @Column(name = "houseAddres")
-    private String houseAddres;
+    @Column(name = "houseNumber")
+    private Integer houseNumber;
 
     @Column(name = "description")
     @Lob
     private String description;
+
+    @OneToMany(mappedBy = "rooms", cascade = CascadeType.ALL)
+    private Set<Room> rooms = new HashSet<>();
+
+    public Hotel() {
+
+    }
+
+    public Hotel(String name, String country, String city, String street, Integer houseNumber, String description) {
+        this.name = name;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getHouseNumber() {
+        return houseNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
 }
