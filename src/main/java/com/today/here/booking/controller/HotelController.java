cefeb3e1125d4c202b202dbe8bd2efb2e9742946
i.tsyuryupa <openingthedoor@yandex.ru>
2023-04-01 20:33:28 +0300

    refactor

--- src/main/java/com/today/here/booking/controller/HotelController.java
+++ src/main/java/com/today/here/booking/controller/HotelController.java
@@ -29,59 +29,6 @@ public class HotelController {
     ReservationRepository reservationRepository;
     @Autowired
     RoomRepository roomRepository;
-  /*  @PostMapping("/hotel/find")
-    public ResponseEntity<?> getAllHotelFreeBetweenDate(@RequestBody FindHotel findHotel) {
-        try {
-            List<Room> rooms = roomRepository.findAll();
-            List<Reservation> reservations = reservationRepository.findAll();
-
-            if (rooms.isEmpty()) {
-                return new ResponseEntity<>(null, HttpStatus.OK);
-            } else if (findHotel.getDateIn().isAfter(findHotel.getDateOut())) {
-                return new ResponseEntity<>("DataIn after dateOut", HttpStatus.BAD_REQUEST);
-            } else {
-                LocalDate dateIn = findHotel.getDateIn();
-                LocalDate dateOut = findHotel.getDateOut();
-                // find all appropriate rooms
-                for (Reservation reservation : reservations) {
-                    LocalDate reservDateIn = reservation.getDateIn();
-                    LocalDate reservDateOut = reservation.getDateOut();
-                    if (dateIn.isAfter(reservDateIn) && dateIn.isBefore(reservDateOut)) {
-                        rooms.remove(reservation.getRoom());
-
-                    } else if (dateOut.isAfter(reservDateIn) && dateOut.isBefore(reservDateOut)) {
-                        rooms.remove(reservation.getRoom());
-
-                    } else if (reservDateOut.isAfter(dateIn) && reservDateOut.isBefore(dateOut)) {
-                        rooms.remove(reservation.getRoom());
-
-                    } else if (reservDateIn.isAfter(dateIn) && reservDateIn.isBefore(dateOut)) {
-                        rooms.remove(reservation.getRoom());
-
-                    } else if (reservDateIn.equals(dateIn) && reservDateOut.equals(dateOut)) {
-                        rooms.remove(reservation.getRoom());
-
-                    }
-                }
-                Set<Hotel> hotelHashSet = new HashSet<>();
-
-                for (Room room: rooms) {
-                    hotelHashSet.add(room.getHotel());
-                }
-
-                List<Hotel> hotels = new ArrayList<>();
-                for (Hotel hotel: hotelHashSet) {
-                    if (hotel.getCity().equalsIgnoreCase(findHotel.getCity()) ){
-                        hotels.add(hotel);
-                    }
-                }
-                return new ResponseEntity<List<Hotel>>(hotels, HttpStatus.OK);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }*/
 
     @GetMapping("/hotel/find")
     public ResponseEntity<?> geTAllHotelFreeBetweenDate(@RequestParam(name = "dateIn") LocalDate dateIn, @RequestParam(name = "dateOut") LocalDate dateOut, @RequestParam(name = "city") String city) {
