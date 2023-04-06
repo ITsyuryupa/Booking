3b9d11f771ad2d8561c0995d41f71b3aed8317ac
i.tsyuryupa <openingthedoor@yandex.ru>
2023-04-06 20:12:40 +0300

    version 1.0

---
+++ src/test/java/com/today/here/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.today.here.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
