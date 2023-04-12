9e5beb749d98395195d6a93825e979f2755f1814
i.tsyuryupa <openingthedoor@yandex.ru>
2023-04-12 11:15:15 +0300

    add model FileRoomRepository

---
+++ src/main/java/com/today/here/booking/repository/FileRoomRepository.java
@@ -0,0 +1,17 @@
+package com.today.here.booking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.today.here.booking.model.FileRoom;
+
+import java.util.List;
+
+@Repository
+public interface FileRoomRepository extends JpaRepository<FileRoom, String> {
+
+    @Query(value = "SELECT id FROM file_room  WHERE room_id = ?1", nativeQuery = true)
+    List<String> findAllIdByHotelId(@Param("id") Long room_id);
+}
